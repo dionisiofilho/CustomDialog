c581b59facc86312fd1b0ddce50f27c730e02265
dionisio.filho <dionisio.filho@compline.com.br>
2018-10-30 11:32:39 -0200

    testing

--- app/src/main/java/com/dionisiofilho/customdialog/MainActivity.kt
+++ app/src/main/java/com/dionisiofilho/customdialog/MainActivity.kt
@@ -2,11 +2,20 @@ package com.dionisiofilho.customdialog
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dionisiofilho.dialog.custom.CustomDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CustomDialog.Builder(this)
+                .setTitle("Título")
+                .setMessage("Mensagem")
+                .setDialogType(CustomDialog.CustomDialogType.ERROR_ALERT)
+                .listenerConfirm { }
+                .listenerCancel { }
+                .build()
     }
 }
